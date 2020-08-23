62d05b23a19d2d7eda20255b9c366bcb4eae93b8
rahulsai143 <kandularahulreddy96@gmail.com>
2020-08-24 04:39:15 +0530

    Initial Commit for the spring boot project

---
+++ src/main/java/com/couponzz/couponsfactory/couponsapp/CouponsappApplication.java
@@ -0,0 +1,13 @@
+package com.couponzz.couponsfactory.couponsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponsappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/couponzz/couponsfactory/couponsapp/CouponsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couponzz.couponsfactory.couponsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
